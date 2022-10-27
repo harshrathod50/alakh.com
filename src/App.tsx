dc35bb6e8047e80c246648186625270083bb6048
Harsh Rathod <hrathore50@ymail.com>
2022-10-27 23:18:29 +0530

    Auto fix all linting issues.

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,22 @@
-import './App.css'
+import "./App.css";
 
 function App() {
   return (
     <>
       <h1 className="text-3xl font-bold">Hello, World!</h1>
       <p>This is the personal web portfolio of Alakh Karmarkar.</p>
-      <h1 lang="hi" className="text-3xl font-bold">नमस्ते, दुनिया!</h1>
+      <h1 lang="hi" className="text-3xl font-bold">
+        नमस्ते, दुनिया!
+      </h1>
       <p lang="hi">यह अलख करमरकर का निजी वेब पोर्टफोलियो है।</p>
-      <h1 lang="ja" className="text-3xl font-bold">アラフ・カルマルカール</h1>
-      <p lang="ja">これは、アラフ・カルマルカールの個人的な Web ポートフォリオです。</p>
+      <h1 lang="ja" className="text-3xl font-bold">
+        アラフ・カルマルカール
+      </h1>
+      <p lang="ja">
+        これは、アラフ・カルマルカールの個人的な Web ポートフォリオです。
+      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
