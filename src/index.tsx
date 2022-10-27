440f8f51b1d0bdc80f019d650429b93fa531b155
Harsh Rathod <hrathore50@ymail.com>
2022-10-27 21:25:37 +0530

    Installed and integrated custom font-families.

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,34 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import '@fontsource/roboto-mono/300.css';
+import '@fontsource/roboto-mono/400.css';
+import '@fontsource/roboto-mono/500.css';
+import '@fontsource/roboto-mono/700.css';
+import '@fontsource/roboto-serif/300.css';
+import '@fontsource/roboto-serif/400.css';
+import '@fontsource/roboto-serif/500.css';
+import '@fontsource/roboto-serif/700.css';
+import '@fontsource/noto-sans-devanagari/300.css';
+import '@fontsource/noto-sans-devanagari/400.css';
+import '@fontsource/noto-sans-devanagari/500.css';
+import '@fontsource/noto-sans-devanagari/700.css';
+import '@fontsource/noto-serif-devanagari/300.css';
+import '@fontsource/noto-serif-devanagari/400.css';
+import '@fontsource/noto-serif-devanagari/500.css';
+import '@fontsource/noto-serif-devanagari/700.css';
+import '@fontsource/noto-sans-jp/300.css';
+import '@fontsource/noto-sans-jp/400.css';
+import '@fontsource/noto-sans-jp/500.css';
+import '@fontsource/noto-sans-jp/700.css';
+import '@fontsource/noto-serif-jp/300.css';
+import '@fontsource/noto-serif-jp/400.css';
+import '@fontsource/noto-serif-jp/500.css';
+import '@fontsource/noto-serif-jp/700.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
